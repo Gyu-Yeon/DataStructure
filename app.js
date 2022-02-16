e2491568d545a900dcb349e076302b721f7c83f1
Gyu-Yeon <idsopy@nate.com>
2022-02-16 23:18:29 +0900

    day 2

--- app.js
+++ app.js
@@ -28,3 +28,25 @@ let t3 = performance.now();
 addupTo2(1000000000);
 let t4 = performance.now();
 console.log(`Time Elasped: ${(t4 - t3) / 1000} seconds.`);
+
+//Space complexity
+
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+console.log(sum([1, 2, 3, 4, 5]));
+
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+}
+
+console.log(double([1, 2, 3]));
