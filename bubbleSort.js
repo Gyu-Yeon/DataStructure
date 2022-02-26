ad5aa83abdf31f5732369fe822c671966b15d6a8
Gyu-Yeon <idsopy@nate.com>
2022-02-27 01:38:54 +0900

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,48 @@
+// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘
+// 인접한 2개의 레코드를 비교하여 크기가 순서대로 되어 있지 않으면 서로 교환한다.
+// 선택 정렬과 기본 개념이 유사하다.
+
+// SWAP
+// ES5
+function swap1(arr, idx1, idx2) {
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx1];
+  arr[idx2] = temp;
+}
+// ES2015;
+const swap2 = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[(idx2, arr[idx1])]];
+};
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
