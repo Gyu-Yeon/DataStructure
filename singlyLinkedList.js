bb6cfe214c4b6bb4c24cbead395fea1992d29e71
Gyu-Yeon <idsopy@nate.com>
2022-03-09 21:31:06 +0900

    singlyLinkedList-1

---
+++ singlyLinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    if (this.length == 0) {
+      this.head = new Node(val);
+      this.tail = this.head;
+      this.length++;
+    } else {
+      this.tail.next = new Node(val);
+      this.tail = this.tail.next;
+      this.length++;
+    }
+  }
+}
+
+let list = new SinglyLinkedList();
