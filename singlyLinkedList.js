f05da831ee981f1c1667a29dc352ae861b842750
Gyu-Yeon <idsopy@nate.com>
2022-03-10 22:37:02 +0900

    singlyLinkedList-2

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -12,15 +12,33 @@ class SinglyLinkedList {
     this.length = 0;
   }
   push(val) {
-    if (this.length == 0) {
-      this.head = new Node(val);
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
       this.tail = this.head;
-      this.length++;
     } else {
-      this.tail.next = new Node(val);
-      this.tail = this.tail.next;
-      this.length++;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
   }
 }
 
