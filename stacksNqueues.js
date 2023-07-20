dee4f173f5b34089b7df37cc51b7e8aff5ec81a2
Gyu-Yeon <idsopy@nate.com>
2023-07-21 00:16:31 +0900

    1

--- stacksNqueues.js
+++ stacksNqueues.js
@@ -1,6 +1,16 @@
 function factorial(num) {
-    if (num === 1) {
-      return 1;
-    }
-    return num * factorial(num - 1);
+  if (num === 1) {
+    return 1;
   }
+  return num * factorial(num - 1);
+}
+
+let stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
+
+stack.pop();
+
+console.log(stack);
